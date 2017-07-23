9798369de35ba3cd07a5f0365811ef484ee55583
rajvansia <vansia112@gmail.com>
2017-07-23 19:20:08 -0400

    bottom  border

--- components/CareList/styles.js
+++ components/CareList/styles.js
@@ -2,28 +2,52 @@ import React,{Component} from 'react';
 import{ StyleSheet} from 'react-native'
 
 export const styles = StyleSheet.create({
-  transparentButton: {
-    marginTop: 30,
+//   transparentButton: {
+//     marginTop: 30,
+//     borderColor: 'black',
+//     borderWidth: 2,
+//     padding: 100,
+//     marginLeft: 20,
+//     marginRight: 20,
+//
+// },
+  card: {
+    padding: 20,
     borderColor: 'black',
     borderWidth: 2,
-    padding: 100,
-    marginLeft: 20,
-    marginRight: 20,
+    margin: 10,
+    borderBottomColor: 'black',
+    borderBottomWidth: 5,
+  },
+
+  valueArea: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 2,
+  },
+
+  image: {
+    width: 69,
+    height: 69,
+    justifyContent:"center",
 
 },
-card: {
-  backgroundColor: 'blue',
-  padding: 20,
-  borderColor: 'black',
-  borderWidth: 2,
-  margin: 10
-},
-image: {
-  width: 69,
-  height: 69,
-  justifyContent:"center",
+  container: {
+    flex: 1,
+    paddingBottom: 50,
+    paddingTop: 50,
+    backgroundColor: '#d9e3f0',
+  },
+  title: {
+    fontSize: 30,
+    paddingBottom: 10
+  },
+  value: {
+    fontSize: 25,
+    paddingBottom: 20,
+    alignItems:"center",
+    justifyContent:"center",
 
-},
+  },
 });
 
 module.exports = styles
--- components/Ui/HealthCard.js
+++ components/Ui/HealthCard.js
@@ -12,7 +12,7 @@ import styles from '../CareList/styles.js'
 
 const Title = (props) => {
   return (
-    <Text>
+    <Text style={styles.title}>
       {props.title}
     </Text>
   );
@@ -26,24 +26,27 @@ const CardImage = (props) => {
 
 const CardValue = (props) => {
   return(
-    <Text>
+  <View style={styles.valueArea}>
+    <Text style={styles.value}>
       {props.value}
     </Text>
+  </View>
   )
 }
 
 const CardDate = (props) => {
   return(
     <Text>
-      Last updated {props.date}
+      Last updated {props.date}                               Past Data
     </Text>
   )
 }
 
 const TotalCard = (props) => {
   return(
-    <View style={styles.card}>
-    <Title title= {props.title} />
+    <View style={props.styles.card}>
+    <Title
+      title= {props.title} />
     <CardImage image= {props.image} />
     <CardValue value = {props.value} />
     <CardDate date= {props.date} />
@@ -57,7 +60,9 @@ export default class HealthCard extends Component {
       <ScrollView
             automaticallyAdjustContentInsets={false}
           >
+          <View style = {styles.container} >
             <TotalCard
+              styles = {{card: [styles.card,{backgroundColor:"#2ccce4"}]}}
               title = "Mindfull Eating"
               image = 'https://png.icons8.com/vegan-food/win10/86'
               value = "33 meals"
@@ -65,25 +70,29 @@ export default class HealthCard extends Component {
             />
 
             <TotalCard
+              styles = {{card: [styles.card, {backgroundColor:"#dce775"}]}}
               title = "Mood"
               image = "https://png.icons8.com/mental-state/win10/86"
-              value = "33 meals"
+              value = "Happy"
               date = "9/3/17"
             />
 
             <TotalCard
+              styles = {{card: [styles.card, {backgroundColor:"#ba68c8"}]}}
               title = "Weight"
               image = "https://png.icons8.com/scale/win10/86"
-              value = "33 meals"
+              value = "165 Pounds"
               date = "9/3/17"
             />
 
             <TotalCard
+              styles = {{card: [styles.card, {backgroundColor:"#37d67a"}]}}
               title = "Blood Pressure"
               image = "https://png.icons8.com/heart-health/win10/86"
-              value = "33 meals"
+              value = "120/80"
               date = "9/3/17"
             />
+        </View>
     </ScrollView>
     );
   }
